a636ac639f4c1e3b859275b0629db7714331ec9e
Jevan <jebang@outlook.com>
2022-01-02 22:57:52 +0800

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        orange: "#d96019",
+        "dark-blue": "#274973",
+        "light-blue": "#1e4f92",
+        white: "#ffffff",
+      },
+    },
+  },
+  plugins: [],
+};
